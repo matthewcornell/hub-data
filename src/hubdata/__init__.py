82d7290571c1a49b840ee114449bd4409fbbccd5
matthewcornell <matt@matthewcornell.org>
2025-05-27 14:18:01 -0400

    initial create_hub_schema() implementation - closes #6

--- src/hubdata/__init__.py
+++ src/hubdata/__init__.py
@@ -1,3 +1,6 @@
 from hubdata.hub_config import HubConfig
+from hubdata.hub_schema import create_hub_schema
 
-__all__ = ['HubConfig']
+__all__ = ['HubConfig', 'create_hub_schema']
+
+__version__ = "0.1.0"
