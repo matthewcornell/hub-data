3c5eff9ac6a239622586e9d71ec9cac3127e96ea
matthewcornell <matt@matthewcornell.org>
2025-05-29 14:35:29 -0400

    made app.py a click command with a schema subcommand under the cli group (was main). fixed two type errors. added running commands locally to README.md

--- README.md
+++ README.md
@@ -1,3 +1,60 @@
 # hubdata
 
 Python tools for accessing and working with hubverse Hub data
+
+## running commands locally
+
+All commands should be run from this repository's root, i.e., first do:
+
+```bash
+cd /<path_to_repos>/hub-data/
+```
+
+### app
+
+Print a test hub's schema:
+
+```bash
+uv run hubdata schema test/hubs/example-complex-scenario-hub
+```
+
+### linter (ruff)
+
+```bash
+uv run ruff check
+```
+
+### tests (pytest)
+
+```bash
+uv run pytest
+```
+
+### coverage (coverage)
+
+To generate a text report:
+
+```bash
+uv run --frozen coverage run -m pytest
+uv run --frozen coverage report
+rm .coverage
+```
+
+To generate a html report:
+
+```bash
+uv run --frozen coverage html
+rm -rf htmlcov/index.html
+```
+
+### type checking (mypy)
+
+```bash
+uv tool run mypy . --ignore-missing-imports --disable-error-code=attr-defined
+```
+
+->
+
+	src/hubdata/hub_schema.py:31: error: Argument 2 to "_columns_for_model_task" has incompatible type "tuple[tuple[str, Any]] | None"; expected "tuple[tuple[str, Any]]"  [arg-type]
+	src/hubdata/hub_schema.py:41: error: Argument 1 to "_pa_type_for_hub_type" has incompatible type "str | None"; expected "str"  [arg-type]
+	Found 2 errors in 1 file (checked 10 source files)
--- src/hubdata/app.py
+++ src/hubdata/app.py
@@ -1,32 +1,62 @@
+import pathlib
+
+import click
 import structlog
-from rich.console import Console
+from rich.console import Console, Group
 from rich.panel import Panel
 
+from hubdata import HubConfig, create_hub_schema
 from hubdata.logging import setup_logging
 
 setup_logging()
 logger = structlog.get_logger()
 
 
-def main():
-    """hubdata starting point."""
-    logger.info('starting hubdata...')
+@click.group()
+def cli():
+    pass
 
+
+@cli.command(name='schema')
+@click.argument('hub_dir', type=click.Path(file_okay=False, exists=True, path_type=pathlib.Path))
+def print_schema(hub_dir):
+    """
+    A subcommand that prints the schema for `hub_dir`.
+    """
+    hub_config = HubConfig(hub_dir)
+    schema = create_hub_schema(hub_config.tasks)
+    rounds = hub_config.tasks['rounds']
     console = Console()
+
+    # create the hub_dir group lines
+    hub_dir_lines = ['[b]hub_dir[/b]:',
+                     f'- {hub_dir}']
+
+    # create the rounds group lines
+    rounds_lines = [f'\n[b]rounds[/b] ({len(rounds)}):']
+    for the_round in rounds:
+        plural = 's' if len(the_round['model_tasks']) > 1 else ''
+        rounds_lines.append(f'- [green]{the_round['round_id']}[/green] ({len(the_round['model_tasks'])} '
+                            f'model task{plural})')
+
+    # create the schema group lines
+    schema_lines = ['\n[b]schema[/b]:']
+    for field in sorted(schema, key=lambda _: _.name):
+        schema_lines.append(f'- [green]{field.name}[/green]: [bright_magenta]{field.type}[/bright_magenta]')
+
+    # finally, print a Panel containing all the groups
     console.print(
         Panel(
-            ':tada: Hello from the hubdata Python package!',
+            Group(Group(*hub_dir_lines), Group(*rounds_lines), Group(*schema_lines)),
             border_style='green',
             expand=False,
-            padding=(1, 4),
-            subtitle='[italic]created by pyprefab[/italic]',
+            padding=(1, 2),
+            subtitle='[italic]hubdata[/italic]',
             subtitle_align='right',
-            title='hubdata',
-            title_align='left',
-        )
+            title=f'[bright_red]{hub_dir.name}[/bright_red]',
+            title_align='left')
     )
 
 
 if __name__ == '__main__':
-    main()
-
+    cli()
